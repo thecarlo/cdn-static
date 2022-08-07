349394951ffbcbac2dbd2e0c95bbf559fcace978
Carlo van Wyk <5310264+thecarlo@users.noreply.github.com>
2022-08-07 20:54:25 +1000

    remove gzip

--- compress.sh
+++ compress.sh
@@ -6,7 +6,6 @@ IFS=$'\n'
 for FILE in $( find dist -type f \( -iname "*.html" \) ); do
     echo -e "Compressing ${FILE}\n"
     brotli ${FILE} -o ${FILE}.br -f --best;
-    gzip -c ${FILE} > ${FILE}.gz -f --best;
 done
 
 IFS=$OLDIFS
